3e6a72f5940315348202f8e5e9d6e908709998ee
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-13 06:24:10 -1000

    Update db.ts

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -8,8 +8,11 @@ const firebaseConfig = {
   storageBucket: "boom-e4528.appspot.com",
   messagingSenderId: "649424857876",
   appId: "1:649424857876:web:7f71c7fb489862539a3001",
-  databaseUrl: "https://boom-e4528-default-rtdb.firebaseio.com",
+  // databaseUrl: "https://boom-e4528-default-rtdb.firebaseio.com",
 };
+
+// const urlparams = new URLSearchParams(window.location.search);
+// const roomId = urlparams.get("id");
 
 export const app = initializeApp(firebaseConfig);
 
