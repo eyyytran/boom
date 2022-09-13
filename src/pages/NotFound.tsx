333a15663d43ecb03995dd6056e0f050edadede5
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-13 04:37:32 -1000

    Create NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from "react";
+import Component from "../components/Component";
+
+type Styles = {
+  static: string;
+};
+
+const styles = {} as Styles;
+
+styles.static = "fixed inset-0 bg-neutral-200";
+
+export default function NotFound() {
+  return (
+    <Component id="NotFound">
+      <div className={`${styles.static}`}>
+        <div className="flex flex-col justify-start h-full">
+          <span className="">404</span>
+          <span className="">Page Not Found</span>
+        </div>
+      </div>
+    </Component>
+  );
+}
