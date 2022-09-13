539271591606475fce10bb89451090e38d04fabd
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-13 06:24:02 -1000

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,8 +2,10 @@ import React, { MutableRefObject, ReactElement, useEffect, useRef } from "react"
 
 import subscriberSlice from "../store/subscriberSlice";
 
-import { collection, getDocs } from "firebase/firestore";
-import { app, db } from "../firebase/db";
+// import { collection, getDocs } from "firebase/firestore";
+// import { app, db } from "../firebase/db";
+
+import { getCalls, getUsers } from "../firebase/pc";
 
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
@@ -36,23 +38,31 @@ export default function Gallery({ className = "" }: Props) {
 
   styles.dynamic = className;
 
-  const getUsers = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    return querySnapshot;
-  };
+  // const getUsers = async () => {
+  //   const querySnapshot = await getDocs(collection(db, "users"));
+  //   return querySnapshot;
+  // };
+
+  // const setUsers = async () => {
+  //   const users = await getUsers();
+  //   users.forEach(doc => dispatch(subscriber.actions.addUser(doc)));
+  // };
 
   const setUsers = async () => {
     const users = await getUsers();
-    users.forEach(doc => dispatch(subscriber.actions.addUser(doc)));
+    const arr: any = [];
+    users.forEach(doc => arr.push(doc.data()));
+    dispatch(subscriber.actions.setUsers(arr));
   };
 
   useEffect(() => {
     setUsers();
+    console.log(getCalls());
   }, []);
 
   useEffect(() => {
     // subscriber.state.users.forEach((user: any) => console.log(user));
-    subscriber.state.users.forEach((user: any) => console.log({ name: user._document.data.value.mapValue.fields.name.stringValue }));
+    // subscriber.state.users.forEach((user: any) => console.log({ name: user._document.data.value.mapValue.fields.name.stringValue }));
   }, [subscriber.state.users]);
 
   return (
