64087e5a675b5bf93fdb6cd8da2616700daedab0
eyyytran <ltatran94@gmail.com>
2022-09-13 23:46:13 -0400

    fixeed?

--- client/src/server/firebase.js
+++ client/src/server/firebase.js
@@ -1,9 +1,19 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { child, getDatabase, ref, get, set, push } from 'firebase/database'
+import { child, getDatabase, ref, get, set } from 'firebase/database'
 import firebase from 'firebase/compat/app'
-import { firebaseConfig } from './config'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAXZOpO_nf5jKExRW-zMk8JWSXZalVK49A',
+    authDomain: 'boom-test-fe706.firebaseapp.com',
+    projectId: 'boom-test-fe706',
+    storageBucket: 'boom-test-fe706.appspot.com',
+    messagingSenderId: '447496272705',
+    appId: '1:447496272705:web:6da20aee0b46b90f45ffc0',
+    measurementId: 'G-G9XHREWSFP',
+    databaseURL: 'https://boom-test-fe706-default-rtdb.firebaseio.com/',
+}
 
 firebase.initializeApp(firebaseConfig)
 const app = initializeApp(firebaseConfig)
@@ -18,11 +28,20 @@ const roomId = urlparams.get('id')
 
 console.log(firepadRef)
 
+<<<<<<< HEAD
 if (roomId) {
     firepadRef = child(db, roomId)
 } else {
     firepadRef = push(db)
     window.history.replaceState(null, 'Meet', '?id=' + firepadRef.key)
 }
+=======
+// if (roomId) {
+//     firepadRef = firepadRef.child(roomId)
+// } else {
+//     firepadRef = firepadRef.push()
+//     window.history.replaceState(null, 'Meet', '?id=' + firepadRef.key)
+// }
+>>>>>>> parent of ca282ad (fixing syntax)
 
 export default firepadRef
