a47229c1c465b17fcb54e3ce70a4545e814857d3
eyyytran <ltatran94@gmail.com>
2022-09-14 11:21:23 -0400

    happily creating rooms

--- client/src/server/firebase.js
+++ client/src/server/firebase.js
@@ -1,28 +1,31 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-import { child, getDatabase, ref, push, get, set } from 'firebase/database'
-import firebase from 'firebase/compat/app'
+import { getFirestore, collection, doc, addDoc } from 'firebase/firestore'
 import { firebaseConfig } from './config'
 
-firebase.initializeApp(firebaseConfig)
 const app = initializeApp(firebaseConfig)
-export const firestore = getFirestore(app)
-export const auth = getAuth(app)
-export const db = getDatabase(app)
 
-let firepadRef = ref(db)
+export const db = getFirestore(app)
+export const auth = getAuth(app)
 export const userName = prompt("What's your name?")
+
 const urlparams = new URLSearchParams(window.location.search)
 const roomId = urlparams.get('id')
 
-console.log(firepadRef)
-
-if (roomId) {
-    firepadRef = child(db, roomId)
-} else {
-    firepadRef = push(db)
-    window.history.replaceState(null, 'Meet', '?id=' + firepadRef.key)
+let firepadRef = collection(db, 'rooms')
+const createRoom = async () => {
+    try {
+        const docRef = await addDoc(firepadRef, { name: 'test-room' })
+        firepadRef = doc(db, 'rooms', docRef.id)
+        window.history.replaceState(null, 'Meet', '?id=' + docRef.id)
+    } catch (error) {
+        console.error('error adding document', error)
+    }
 }
 
+if (!roomId) {
+    createRoom()
+} else {
+    firepadRef = doc(db, 'rooms', roomId)
+}
 export default firepadRef
