922b6b4116a2831d60e5c9910102391341e1ee23
Blake Lein <blake.lein@gmail.com>
2022-09-15 10:24:45 -0500

    Changes

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import Component from '../components/Component'
-import Titlebar from '../components/Titlebar'
-import Navbar from '../components/Navbar'
-import SignupForm from '../components/forms/SignupForm'
+import React from "react";
+import Component from "../components/Component";
+import Titlebar from "../components/Titlebar";
+import Navbar from "../components/Navbar";
+import SignupForm from "../components/forms/SignupForm";
 
 type Styles = {
-    static: string
-}
+  static: string;
+};
 
-const styles = {} as Styles
+const styles = {} as Styles;
 
-styles.static = 'fixed inset-0 bg-neutral-200'
+styles.static = "fixed inset-0 bg-neutral-200";
 
 const Signup = () => {
-    return (
-        <Component id='Signup'>
-            <div className='flex flex-col justify-start h-full'>
-                <Titlebar className='shrink-0' />
-                <div className='flex h-full overflow-y-clip overflow-x-auto snap-x snap-mandatory no-scrollbar'>
-                    <SignupForm />
-                </div>
-                <Navbar className='shrink-0' />
-            </div>
-        </Component>
-    )
-}
+  return (
+    <Component id="Signup">
+      <div className="flex flex-col justify-start h-full">
+        <Titlebar className="shrink-0" />
+        <div className="flex h-full overflow-y-clip overflow-x-auto snap-x snap-mandatory no-scrollbar">
+          <SignupForm />
+        </div>
+        {/* <Navbar className='shrink-0' /> */}
+      </div>
+    </Component>
+  );
+};
 
-export default Signup
+export default Signup;
--- client/src/server/firebase.js
+++ client/src/server/firebase.js
@@ -1,51 +1,51 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {
-    getFirestore,
-    collection,
-    doc,
-    addDoc,
-    updateDoc,
-    arrayUnion,
-} from 'firebase/firestore'
-import { firebaseConfig } from './config'
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  updateDoc,
+  arrayUnion,
+} from "firebase/firestore";
+import { firebaseConfig } from "./config";
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app)
-export const auth = getAuth(app)
-export const userName = prompt("What's your name?")
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const userName = "Blake";
 
-const urlparams = new URLSearchParams(window.location.search)
-const roomId = urlparams.get('id')
+const urlparams = new URLSearchParams(window.location.search);
+export const roomId = urlparams.get("id");
 
-let firepadRef = collection(db, 'rooms')
+let firepadRef = collection(db, "rooms");
 
-const createRoom = async () => {
-    try {
-        const docRef = await addDoc(firepadRef, { primaryUser: userName })
-        firepadRef = doc(db, 'rooms', docRef.id)
-        window.history.replaceState(null, 'Meet', '?id=' + docRef.id)
-    } catch (error) {
-        console.error('error adding document', error)
-    }
-}
+export const createRoom = async () => {
+  try {
+    const docRef = await addDoc(firepadRef, { primaryUser: userName });
+    firepadRef = doc(db, "rooms", docRef.id);
+    window.history.replaceState(null, "Meet", "?id=" + docRef.id);
+  } catch (error) {
+    console.error("error adding document", error);
+  }
+};
 
-const updateRoom = async () => {
-    try {
-        firepadRef = doc(db, 'rooms', roomId)
-        await updateDoc(firepadRef, {
-            participants: arrayUnion(userName),
-        })
-        window.history.replaceState(null, 'Meet', '?id=' + roomId)
-    } catch (error) {
-        console.error('error adding a participant', error)
-    }
-}
+export const updateRoom = async () => {
+  try {
+    firepadRef = doc(db, "rooms", roomId);
+    await updateDoc(firepadRef, {
+      participants: arrayUnion(userName),
+    });
+    window.history.replaceState(null, "Meet", "?id=" + roomId);
+  } catch (error) {
+    console.error("error adding a participant", error);
+  }
+};
 
-if (!roomId) {
-    createRoom()
-} else {
-    updateRoom()
-}
-export default firepadRef
+// if (!roomId) {
+//   createRoom();
+// } else {
+//   updateRoom();
+// }
+export default firepadRef;
