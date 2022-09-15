4cb8c0c16295c13f9643efd1360cae0b082aea00
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-15 05:17:53 -1000

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,10 +2,9 @@ import React, { MutableRefObject, ReactElement, useEffect, useRef } from "react"
 
 import subscriberSlice from "../store/subscriberSlice";
 
-// import { collection, getDocs } from "firebase/firestore";
-// import { app, db } from "../firebase/db";
-
-import { addCall, getCalls, getUsers } from "../firebase/pc";
+import { pc } from "../firebase/pc";
+import { addUser } from "../firebase/user";
+import { createOffer } from "../firebase/offer";
 
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
@@ -24,6 +23,8 @@ type Styles = {
   dynamic?: string;
 };
 
+const getUserMedia = async () => await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+
 const styles = {} as Styles;
 
 styles.static = "shrink-0 w-full h-full p-2 md:p-3 lg:p-4";
@@ -36,43 +37,16 @@ export default function Gallery({ className = "" }: Props) {
 
   const dispatch = useDispatch();
 
+  useEffect(() => {}, []);
+
   styles.dynamic = className;
-
-  // const getUsers = async () => {
-  //   const querySnapshot = await getDocs(collection(db, "users"));
-  //   return querySnapshot;
-  // };
-
-  // const setUsers = async () => {
-  //   const users = await getUsers();
-  //   users.forEach(doc => dispatch(subscriber.actions.addUser(doc)));
-  // };
-
-  const setUsers = async () => {
-    const users = await getUsers();
-    const arr: any = [];
-    users.forEach(doc => arr.push(doc.data()));
-    dispatch(subscriber.actions.setUsers(arr));
-  };
-
-  useEffect(() => {
-    setUsers();
-  }, []);
-
-  useEffect(() => {
-    // subscriber.state.users.forEach((user: any) => console.log(user));
-    // subscriber.state.users.forEach((user: any) => console.log({ name: user._document.data.value.mapValue.fields.name.stringValue }));
-  }, [subscriber.state.users]);
 
   return (
     <Component id="Gallery">
       <div className={`${styles.static} ${styles.dynamic}`}>
         <Container>
           <div className="flex portrait:flex-col justify-center items-center h-full gap-2 md:gap-3 lg:gap-4">
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+            <Video srcObject={"srcObject"} />
           </div>
         </Container>
       </div>
