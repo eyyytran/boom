fbbc57e04211d55826017a8608fde82cfb83877f
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-15 05:18:06 -1000

    Update pc.ts

--- src/firebase/pc.ts
+++ src/firebase/pc.ts
@@ -1,62 +1,32 @@
-import { collection, getDocs, addDoc } from "firebase/firestore";
-import { db } from "./db";
+import { addOffer } from "./offer";
 
-const servers = {
-  iceServers: [
-    {
-      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
-    },
-  ],
+import { v4 as uuid } from "uuid";
+
+export const pc = new RTCPeerConnection({
+  iceServers: [{ urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"] }],
   iceCandidatePoolSize: 10,
-};
+});
 
-export const getUsers = async () => {
-  const querySnapshot = await getDocs(collection(db, "users"));
-  return querySnapshot;
-};
+export const uid = uuid();
 
-export const getCalls = async () => {
-  const querySnapshot = await getDocs(collection(db, "calls"));
-  return querySnapshot;
-};
-
-export const addCall = async (data: any) => {
-  try {
-    const docRef = await addDoc(collection(db, "calls"), data);
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
-
-export const addOffer = async (data: any) => {
-  try {
-    const docRef = await addDoc(collection(db, "calls"), addDoc(collection(db, "offers"), data));
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
-
-export const addAnswer = async (data: any) => {
-  try {
-    const docRef = await addDoc(collection(db, "calls"), addDoc(collection(db, "answers"), data));
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
-
-export const pc = new RTCPeerConnection(servers);
-
-export const createOffer = async () => {
-  const offerDescription = await pc.createOffer();
-  await pc.setLocalDescription(offerDescription);
-
-  const offer = {
-    sdp: offerDescription.sdp,
-    type: offerDescription.type,
-  };
-
-  await addCall({ offer });
+pc.onicecandidate = e => {
+  if (!e.candidate) return;
+  console.log(e.candidate);
+  addOffer({
+    address: e.candidate.address,
+    candidate: e.candidate.candidate,
+    component: e.candidate.component,
+    foundation: e.candidate.foundation,
+    port: e.candidate.port,
+    priority: e.candidate.priority,
+    protocol: e.candidate.protocol,
+    relatedAddress: e.candidate.relatedAddress,
+    relatedPort: e.candidate.relatedPort,
+    sdpMLineIndex: e.candidate.sdpMLineIndex,
+    sdpMid: e.candidate.sdpMid,
+    tcpType: e.candidate.tcpType,
+    type: e.candidate.type,
+    usernameFragment: e.candidate.usernameFragment,
+    uid: uid,
+  });
 };
