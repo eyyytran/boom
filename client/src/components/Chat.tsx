52521100eaa5e588c36b46bfba7624e102c5f705
eyyytran <ltatran94@gmail.com>
2022-09-15 14:54:22 -0400

    lame chat with hard coded sentBy and user status

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,42 +1,150 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useEffect } from 'react'
 
-import Component from "../components/Component";
-import Container from "../layout/Container";
-import Message from "../components/Message";
+import Component from '../components/Component'
+import Container from '../layout/Container'
+import Message from '../components/Message'
+import {
+    arrayUnion,
+    doc,
+    updateDoc,
+    onSnapshot,
+    getDoc,
+} from 'firebase/firestore'
+import { db } from '../server/firebase'
 
 type Props = {
-  className?: string | null;
-};
+    className?: string | null
+}
 
 type Styles = {
-  static: string;
-  dynamic?: string | null;
-};
+    static: string
+    dynamic?: string | null
+}
 
-const styles = {} as Styles;
+interface FirebaseMessage {
+    sentBy: string
+    content: string
+    timeStamp: number
+}
 
-styles.static = "shrink-0 w-full h-full p-2 md:p-3 lg:p-4";
+const styles = {} as Styles
+
+styles.static = 'shrink-0 w-full h-full p-2 md:p-3 lg:p-4'
 
 export default function Chat({ className = null }: Props) {
-  styles.dynamic = className;
-  return (
-    <Component id="Chat">
-      <div className={`${styles.static} ${styles.dynamic}`}>
-        <Container>
-          <div className="flex flex-col h-full gap-2">
-            <div className="flex flex-col h-full gap-2 overflow-y-auto no-scrollbar">
-              <Message username={"Username"} message={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?"} origin={"user"} />
-              <Message username={"Username"} message={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?"} origin={"participant"} />
-              <Message username={"Username"} message={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?"} origin={"participant"} />
-              <Message username={"Username"} message={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?"} origin={"user"} />
-              <Message username={"Username"} message={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?"} origin={"participant"} />
+    styles.dynamic = className
+    const [message, setMessage] = useState<string>('')
+    const [chat, setChat] = useState<Array<FirebaseMessage>>([])
+
+    const handleMessage = (e: any) => {
+        const value = e.target.value
+        setMessage(value)
+    }
+
+    const urlparams = new URLSearchParams(window.location.search)
+    const roomId: any = urlparams.get('id')
+
+    const roomRef = doc(db, 'rooms', roomId)
+
+    useEffect(() => {
+        if (!roomId) return
+        const unsubscribe = onSnapshot(doc(db, 'rooms', roomId), doc => {
+            const result = doc.data()
+            setChat(result?.messages)
+        })
+    }, [])
+
+    const sendMessage = async (dataToSend: {}) => {
+        try {
+            await updateDoc(roomRef, {
+                messages: arrayUnion(dataToSend),
+            })
+            console.log('message sent')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleSubmit = () => {
+        const dataToSend = {
+            sentBy: 'Blake',
+            timeStamp: new Date().getTime(),
+            content: message,
+        }
+        console.log('data to send', dataToSend)
+        sendMessage(dataToSend)
+        setMessage('')
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        e.stopPropagation()
+        if (e.key === 'Enter') {
+            handleSubmit()
+        }
+    }
+
+    return (
+        <Component id='Chat'>
+            <div className={`${styles.static} ${styles.dynamic}`}>
+                <Container>
+                    <div className='flex flex-col h-full gap-2'>
+                        <div className='flex flex-col h-full gap-2 overflow-y-auto no-scrollbar'>
+                            {chat?.map(message => {
+                                return (
+                                    <Message
+                                        username={message.sentBy}
+                                        message={message.content}
+                                        origin={'user'}
+                                    />
+                                )
+                            })}
+                            {/* <Message
+                                username={'Username'}
+                                message={
+                                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?'
+                                }
+                                origin={'user'}
+                            />
+                            <Message
+                                username={'Username'}
+                                message={
+                                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?'
+                                }
+                                origin={'participant'}
+                            />
+                            <Message
+                                username={'Username'}
+                                message={
+                                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?'
+                                }
+                                origin={'participant'}
+                            />
+                            <Message
+                                username={'Username'}
+                                message={
+                                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?'
+                                }
+                                origin={'user'}
+                            />
+                            <Message
+                                username={'Username'}
+                                message={
+                                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ratione laborum neque officia?'
+                                }
+                                origin={'participant'}
+                            /> */}
+                        </div>
+                        <form className='contents' data-lpignore='true'>
+                            <textarea
+                                className='h-8 focus:h-auto p-2 md:p-3 lg:p-4 focus:aspect-square resize-none bg-neutral-200 border border-neutral-400 rounded no-scrollbar'
+                                value={message}
+                                onKeyDown={handleKeyDown}
+                                onChange={handleMessage}
+                            ></textarea>
+                        </form>
+                    </div>
+                </Container>
             </div>
-            <form className="contents">
-              <textarea className="h-8 focus:h-auto p-2 md:p-3 lg:p-4 focus:aspect-square resize-none bg-neutral-200 border border-neutral-400 rounded no-scrollbar"></textarea>
-            </form>
-          </div>
-        </Container>
-      </div>
-    </Component>
-  );
+        </Component>
+    )
 }
