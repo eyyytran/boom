e68d9c7eca73aae6e0dffeff56ef575f20b522f4
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-16 06:52:08 -1000

    Update Boom.tsx

--- client/src/pages/Boom.tsx
+++ client/src/pages/Boom.tsx
@@ -1,4 +1,7 @@
 import React, { ReactElement, useEffect, useRef } from "react";
+
+import { config, useClient, useMicrophoneAndCameraTracks, channelName } from "../server/agora";
+
 import Component from "../components/Component";
 import Titlebar from "../components/Titlebar";
 import Timer from "../components/Timer";
@@ -19,6 +22,10 @@ styles.static = "fixed inset-0 bg-neutral-200";
 export default function Boom() {
   const galleryRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    console.log(galleryRef);
+  }, []);
+
   return (
     <Component id="Boom">
       <div className={`${styles.static}`}>
@@ -26,7 +33,7 @@ export default function Boom() {
           <Titlebar className="shrink-0" />
           <Timer className="shrink-0" />
           <div className="flex h-full overflow-y-clip overflow-x-auto snap-x snap-mandatory no-scrollbar">
-            <Gallery ref={galleryRef} className="snap-center" />
+            <Gallery galleryRef={galleryRef} className="snap-center" />
             {/* <Artboard className="snap-center" /> */}
             <Display className="snap-center" />
             <Chat className="snap-center" />
