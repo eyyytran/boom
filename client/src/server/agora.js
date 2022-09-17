5e74e9381a338c00daf7f9178d36dbf72fc9474d
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-17 09:11:10 -1000

    Update agora.js

--- client/src/server/agora.js
+++ client/src/server/agora.js
@@ -1,7 +1,7 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "871b434bcf5a497ead9d2160e52b815e";
-const token = "007eJxTYJhSdqCB/93qq03XV97d85at1re8ujHi2CJn4UffvEynZWsqMFiYGyaZGJskJaeZJppYmqcmplimGBmaGaSaGiVZGJqm9s9TSeb/oJoc3nyZkZEBAkF8FobcxMw8BgYA9YciSg==";
+const token = "007eJxTYLhkdenE3b3ONx/8CUmzefA8chZDRlDMucu/BFb3Gk7/J3tdgcHC3DDJxNgkKTnNNNHE0jw1McUyxcjQzCDV1CjJwtA0VVxMLXl6unpyuE0nKyMDBIL4LAy5iZl5DAwAO5IiMw==";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
