ba0f25d3f56d4c92695a24ac511c828ff513aa98
Blake Lein <blake.lein@gmail.com>
2022-09-18 11:58:11 -0500

    Added home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Component from "../components/Component";
+import Titlebar from "../components/Titlebar";
+import { Link } from "react-router-dom";
+// import {
+//   faRightToBracket,
+//   faMessage,
+//   faPenToSquare,
+//   faTableCellsLarge,
+//   faVideo,
+//   faCog,
+//   faBars,
+// } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type Props = {};
+
+export default function Home({}: Props) {
+  return (
+    <Component id="Home">
+      <div>
+        <Titlebar className="shrink-0" />
+        {/* <button className="py-2 px-4">
+          <FontAwesomeIcon icon={faBars} className="" />
+        </button> */}
+        <div className="flex flex-col justify-center items-center h-full">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+            Welcome to Boom!
+          </h1>
+          <h3 className="text-center leading-tight tracking-tight text-gray-900 md:text-xl mt-10">
+            You can look at a problem and you can either go, "Oh, this is a
+            problem." Or you can Boom! Blow it up and turn it into something
+            great. You literally Boom the problem.
+          </h3>
+          <div
+            className="flex items-center justify-flex-start px-6 py-8 mx-auto md:h-auto lg:py-0
+            mt-10"
+          >
+            <div className="flex flex-col items-center p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                Create an Account
+              </h1>
+              <Link
+                to="/signup"
+                type="submit"
+                className="w-full text-white bg-indigo-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign Up for Free!
+              </Link>
+            </div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">
+              Or
+            </label>
+            <div className="flex flex-col items-center p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                Existing Account
+              </h1>
+              <Link
+                to="/login"
+                type="submit"
+                className="w-full text-white bg-indigo-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Welcome Back!
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Component>
+  );
+}
--- client/src/server/agora.js
+++ client/src/server/agora.js
@@ -1,7 +1,10 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "871b434bcf5a497ead9d2160e52b815e";
-const token = "007eJxTYLhkdenE3b3ONx/8CUmzefA8chZDRlDMucu/BFb3Gk7/J3tdgcHC3DDJxNgkKTnNNNHE0jw1McUyxcjQzCDV1CjJwtA0VVxMLXl6unpyuE0nKyMDBIL4LAy5iZl5DAwAO5IiMw==";
+
+// This token is only good for four hours. Blake is looking into finding a more permanent solution.
+const token =
+  "007eJxTYLhkdenE3b3ONx/8CUmzefA8chZDRlDMucu/BFb3Gk7/J3tdgcHC3DDJxNgkKTnNNNHE0jw1McUyxcjQzCDV1CjJwtA0VVxMLXl6unpyuE0nKyMDBIL4LAy5iZl5DAwAO5IiMw==";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
