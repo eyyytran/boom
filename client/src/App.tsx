f843909e3306131e3d08f573d6eee263beb02eca
Andrea Tran <105534923+eyyytran@users.noreply.github.com>
2022-09-18 21:44:22 -0400

    Revert "base chat"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,53 +1,31 @@
-import React, { useEffect } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from './server/firebase'
-import appSlice from './store/appSlice'
-import { RootState } from './store'
-import Component from './components/Component'
-import Boom from './pages/Boom'
-import NotFound from './pages/NotFound'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Component from "./components/Component";
+import Boom from "./pages/Boom";
+import NotFound from "./pages/NotFound";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-    const app = {
-        state: useSelector((state: RootState) => state.app),
-        actions: appSlice.actions,
-    }
-
-    const useAuth = () => {
-        const dispatch = useDispatch()
-        useEffect(() => {
-            const unsubscribe = onAuthStateChanged(auth, user => {
-                if (!user) {
-                    dispatch(app.actions.setUser(null))
-                } else {
-                    dispatch(app.actions.setUser(user))
-                }
-            })
-            return unsubscribe
-        }, [])
-    }
-
-    useAuth()
-
-    return (
-        <Component id='App'>
-            <Routes>
-                {/* <Route path="/" element={<Navigate replace to={`/${URL}`} />} /> */}
-                {/* <Route path={URL} element={<Boom />} /> */}
-                <Route path='/' element={<Dashboard />} />
-                <Route path='/boom' element={<Boom />} />
-                <Route path='*' element={<NotFound />} />
-                <Route path='signup' element={<Signup />} />
-                <Route path='login' element={<Login />} />
-            </Routes>
-            <div className=''></div>
-        </Component>
-    )
+  return (
+    <Component id="App">
+      <Routes>
+        {/* <Route path="/" element={<Navigate replace to={`/${URL}`} />} /> */}
+        {/* <Route path={URL} element={<Boom />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/:id" element={<Boom />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Component>
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,67 +1,48 @@
-import React from 'react'
-import {
-    collection,
-    doc,
-    addDoc,
-    updateDoc,
-    arrayUnion,
-} from 'firebase/firestore'
-import { db } from '../server/firebase'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { createRoom, updateRoom, roomId } from "../server/firebase";
+import { Link } from "react-router-dom";
+import Component from "../components/Component";
+import Titlebar from "../components/Titlebar";
 
-type Props = {}
+type Props = {};
+
+type Styles = {
+  static: string;
+};
+
+const styles = {} as Styles;
+
+styles.static = "fixed inset-0 bg-neutral-200";
 
 export default function Dashboard({}: Props) {
-    const urlparams = new URLSearchParams(window.location.search)
-    const roomId: any = urlparams.get('id')
-    var firepadRef = collection(db, 'rooms')
-    const navigate = useNavigate()
-
-    const getUser = () => {
-        const userName = prompt('What is your name?')
-        return userName
-    }
-    const createRoom = async () => {
-        try {
-            const userName = getUser()
-            const docRef = await addDoc(firepadRef, { primaryUser: userName })
-            navigate(`/boom/?id=${docRef.id}`)
-            // window.history.replaceState(null, 'Meet', '?id=' + docRef.id)
-        } catch (error) {
-            console.error('error adding document', error)
-        }
-    }
-
-    const updateRoom = async () => {
-        try {
-            const roomId: any = prompt('Enter the Meeting Key')
-            const userName = getUser()
-            const docRef = doc(db, 'rooms', roomId)
-            await updateDoc(docRef, {
-                participants: arrayUnion(userName),
-            })
-            navigate(`/boom/?id=${roomId}`)
-        } catch (error) {
-            console.error('error adding a participant', error)
-        }
-    }
-    return (
-        <div>
-            <button
-                onClick={async () => {
-                    await createRoom()
-                }}
-            >
-                Start a Game
-            </button>
-            <button
-                onClick={async () => {
-                    updateRoom()
-                }}
-            >
-                Join a Game
-            </button>
-        </div>
-    )
+  return (
+    <Component id="Dashboard">
+      <div className="flex flex-col justify-start h-full">
+        <Titlebar className="shrink-0" />
+        <img
+          src={require("../images/Boom.png")}
+          style={{ height: "250px", width: "250px" }}
+        />
+        <button
+          className="w-full text-white bg-indigo-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          onClick={async () => {
+            if (!roomId) {
+              await createRoom();
+              //@ts-ignore
+              // window.location.href = room;
+            } else {
+              await updateRoom();
+              //@ts-ignore
+              // window.location.href = room;
+            }
+          }}
+        >
+          Start a Game
+        </button>
+        <button className="w-full text-white bg-indigo-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+          Join a Game
+        </button>
+      </div>
+    </Component>
+  );
 }
