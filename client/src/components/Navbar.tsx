c9f8c7c5ddfcdaaac5dff532e4a8b4dc9bd5e60c
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-19 04:04:57 -1000

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,18 +1,15 @@
 import React, { ReactElement } from "react";
 import Component from "./Component";
 import Container from "../layout/Container";
-import {
-  faRightToBracket,
-  faMessage,
-  faPenToSquare,
-  faTableCellsLarge,
-  faVideo,
-  faCog,
-  faBars,
-} from "@fortawesome/free-solid-svg-icons";
+import { faRightToBracket, faMessage, faPenToSquare, faTableCellsLarge, faVideo, faCog, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = {
+  menuButtonRef: any;
+  galleryButtonRef: any;
+  artboardButtonRef: any;
+  chatButtonRef: any;
+  exitButtonRef: any;
   className?: string | null;
 };
 
@@ -25,28 +22,28 @@ const styles = {} as Styles;
 
 styles.static = "p-2 md:p-3 lg:p-4  bg-neutral-300";
 
-export default function Navbar({ className = null }: Props) {
+export default function Navbar({ menuButtonRef, galleryButtonRef, artboardButtonRef, chatButtonRef, exitButtonRef, className = null }: Props) {
   styles.dynamic = className;
   return (
     <Component id="Navbar">
       <div className={`${styles.static} ${styles.dynamic}`}>
         <Container>
           <div className="flex justify-between items-center gap-2 h-full">
-            <button className="py-2 px-4">
+            <button ref={menuButtonRef} className="py-2 px-4">
               <FontAwesomeIcon icon={faBars} className="" />
             </button>
             <div className="flex justify-center items-center gap-2 h-full">
-              <button className="py-2 px-4">
+              <button ref={galleryButtonRef} className="py-2 px-4">
                 <FontAwesomeIcon icon={faVideo} className="text-violet-500" />
               </button>
-              <button className="py-2 px-4">
+              <button ref={artboardButtonRef} className="py-2 px-4">
                 <FontAwesomeIcon icon={faTableCellsLarge} className="" />
               </button>
-              <button className="py-2 px-4">
+              <button ref={chatButtonRef} className="py-2 px-4">
                 <FontAwesomeIcon icon={faMessage} className="" />
               </button>
             </div>
-            <button className="py-2 px-4">
+            <button ref={exitButtonRef} className="py-2 px-4">
               <FontAwesomeIcon icon={faRightToBracket} className="" />
             </button>
           </div>
