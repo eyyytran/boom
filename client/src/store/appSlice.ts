22897bfa278b277488b33e0193a973b9001bb13d
eyyytran <ltatran94@gmail.com>
2022-09-18 22:31:19 -0400

    reverted back to before merge hell

--- client/src/store/appSlice.ts
+++ client/src/store/appSlice.ts
@@ -1,4 +1,4 @@
-import { CaseReducer, CaseReducerActions, createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 import { User } from 'firebase/auth'
 import { auth } from '../server/firebase'
 
