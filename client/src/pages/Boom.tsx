017a73c9e53ede8fa9196793af0c2ec77206d6b9
eyyytran <ltatran94@gmail.com>
2022-09-18 21:58:01 -0400

    fixeed?

--- client/src/pages/Boom.tsx
+++ client/src/pages/Boom.tsx
@@ -1,9 +1,13 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useRef } from "react";
+
+import { config, useClient, useMicrophoneAndCameraTracks, channelName } from "../server/agora";
+
 import Component from "../components/Component";
 import Titlebar from "../components/Titlebar";
 import Timer from "../components/Timer";
 import Gallery from "../components/Gallery";
 import Artboard from "../components/Artboard";
+import Display from "../components/Display";
 import Chat from "../components/Chat";
 import Navbar from "../components/Navbar";
 
@@ -16,6 +20,12 @@ const styles = {} as Styles;
 styles.static = "fixed inset-0 bg-neutral-200";
 
 export default function Boom() {
+  const galleryRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log(galleryRef);
+  }, []);
+
   return (
     <Component id="Boom">
       <div className={`${styles.static}`}>
@@ -23,8 +33,9 @@ export default function Boom() {
           <Titlebar className="shrink-0" />
           <Timer className="shrink-0" />
           <div className="flex h-full overflow-y-clip overflow-x-auto snap-x snap-mandatory no-scrollbar">
-            <Gallery className="snap-center" />
-            <Artboard className="snap-center" />
+            <Gallery galleryRef={galleryRef} className="snap-center" />
+            {/* <Artboard className="snap-center" /> */}
+            <Display className="snap-center" />
             <Chat className="snap-center" />
           </div>
           <Navbar className="shrink-0" />
