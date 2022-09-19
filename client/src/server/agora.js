0906dad5235eeb7382ff1d4d2f88fe55628efba7
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-19 08:28:09 -1000

    Update agora.js

--- client/src/server/agora.js
+++ client/src/server/agora.js
@@ -3,7 +3,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 const appId = "871b434bcf5a497ead9d2160e52b815e";
 
 // This token is only good for four hours. Blake is looking into finding a more permanent solution.
-const token = "007eJxTYDhk+0h0adMElmV73IzsUp1vrWY1aMplUlKO5SiyOWr+4pQCg4W5YZKJsUlScpppoomleWpiimWKkaGZQaqpUZKFoWnqzg6NZMtbmsnr7+9gZWSAQBCfhSE3MTOPgQEAr+0fKA==";
+const token = "007eJxTYHCd8a93YUx3ZfXfRXzhTHc/TGJPZGW7lfHzfeM85bSS1KUKDBbmhkkmxiZJyWmmiSaW5qmJKZYpRoZmBqmmRkkWhqapoZs0kq8yayUrKlQxMEIhiM/CkJuYmcfAAABtwx/U";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
