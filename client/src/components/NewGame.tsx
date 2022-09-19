6f545a08bf6da6ef2a0ba6e515e488410749095c
eyyytran <ltatran94@gmail.com>
2022-09-19 12:06:17 -0400

    add gamestate to db

--- client/src/components/NewGame.tsx
+++ client/src/components/NewGame.tsx
@@ -1,43 +1,51 @@
-import React from "react";
+import React from 'react'
 import {
-  collection,
-  doc,
-  addDoc,
-  updateDoc,
-  arrayUnion,
-} from "firebase/firestore";
-import { db } from "../server/firebase";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
-import userSlice from "../store/userSlice";
+    collection,
+    doc,
+    addDoc,
+    updateDoc,
+    arrayUnion,
+} from 'firebase/firestore'
+import { db } from '../server/firebase'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
+import userSlice from '../store/userSlice'
 
-type Props = {};
+type Props = {}
 
 const NewGame = (props: Props) => {
-  const user = {
-    state: useSelector((state: RootState) => state.user),
-    action: userSlice.actions,
-  };
-  var firepadRef = collection(db, "rooms");
-  const navigate = useNavigate();
-
-  const userName = user.state.userName;
-
-  const createRoom = async () => {
-    try {
-      const docRef = await addDoc(firepadRef, { primaryUser: userName });
-      navigate(`/boom/?id=${docRef.id}`);
-    } catch (error) {
-      console.error("error adding document", error);
+    const user = {
+        state: useSelector((state: RootState) => state.user),
+        action: userSlice.actions,
     }
-  };
-  return (
-    <div>
-      <h1>Click Here to Start a New Game</h1>
-      <button onClick={createRoom}>New Game</button>
-    </div>
-  );
-};
+    var firepadRef = collection(db, 'rooms')
+    const navigate = useNavigate()
 
-export default NewGame;
+    const userName = user.state.userName
+
+    const createRoom = async () => {
+        try {
+            const docRef = await addDoc(firepadRef, {
+                primaryUser: userName,
+                gameState: {
+                    players: arrayUnion({
+                        player: userName,
+                        points: 0,
+                    }),
+                },
+            })
+            navigate(`/boom/?id=${docRef.id}`)
+        } catch (error) {
+            console.error('error adding document', error)
+        }
+    }
+    return (
+        <div>
+            <h1>Click Here to Start a New Game</h1>
+            <button onClick={createRoom}>New Game</button>
+        </div>
+    )
+}
+
+export default NewGame
