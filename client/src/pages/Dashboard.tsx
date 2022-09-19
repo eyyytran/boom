6b6e4667ebeb8908c6f915eeebe4dffd426e6ac6
eyyytran <ltatran94@gmail.com>
2022-09-18 22:02:43 -0400

    Revert "Merge pull request #9 from westmcmillen/revert-8-Andrea"
    This reverts commit 05673a8202fc9d8e890532e1bebf452a0ec7d8c9, reversing
    changes made to 0fde433326e99987e051926d99a0c46ec3313999.

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -6,28 +6,29 @@ import {
     updateDoc,
     arrayUnion,
 } from 'firebase/firestore'
-import { db } from '../server/firebase'
+import { auth, db } from '../server/firebase'
 import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
+import appSlice from '../store/appSlice'
 
 type Props = {}
 
 export default function Dashboard({}: Props) {
-    const urlparams = new URLSearchParams(window.location.search)
-    const roomId: any = urlparams.get('id')
     var firepadRef = collection(db, 'rooms')
     const navigate = useNavigate()
 
-    const getUser = () => {
-        const userName = prompt('What is your name?')
-        return userName
+    const app = {
+        state: useSelector((state: RootState) => state.app),
+        action: appSlice.actions,
     }
+
+    const userName = app.state.userName
+
     const createRoom = async () => {
         try {
-            const userName = getUser()
             const docRef = await addDoc(firepadRef, { primaryUser: userName })
             navigate(`/boom/?id=${docRef.id}`)
-            // window.history.replaceState(null, 'Meet', '?id=' + docRef.id)
         } catch (error) {
             console.error('error adding document', error)
         }
@@ -36,7 +37,6 @@ export default function Dashboard({}: Props) {
     const updateRoom = async () => {
         try {
             const roomId: any = prompt('Enter the Meeting Key')
-            const userName = getUser()
             const docRef = doc(db, 'rooms', roomId)
             await updateDoc(docRef, {
                 participants: arrayUnion(userName),
