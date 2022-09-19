861f6b3ffb95dd661919920a29d415c66cb7dc51
Blake Lein <blake.lein@gmail.com>
2022-09-19 09:37:30 -0500

    Added Dashboard and Home Screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,13 @@ import Boom from "./pages/Boom";
 import NotFound from "./pages/NotFound";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Dash from "./components/Dash";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
+import Settings from "./components/Settings";
+import Join from "./components/Join";
+import Collection from "./components/Collection";
+import NewGame from "./components/NewGame";
 
 import { v4 as uuidv4 } from "uuid";
 
@@ -18,7 +23,13 @@ function App() {
         {/* <Route path="/" element={<Navigate replace to={`/${URL}`} />} /> */}
         {/* <Route path={URL} element={<Boom />} /> */}
         <Route path="/" element={<Home />} />
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="dash" element={<Dash />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="join" element={<Join />} />
+          <Route path="new" element={<NewGame />} />
+          <Route path="collection" element={<Collection />} />
+        </Route>
         <Route path="/:id" element={<Boom />} />
         <Route path="*" element={<NotFound />} />
         <Route path="signup" element={<Signup />} />
---
+++ client/src/components/Join.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Join = (props: Props) => {
+  return (
+    <div>
+      <h1>Join</h1>
+    </div>
+  );
+};
+
+export default Join;
