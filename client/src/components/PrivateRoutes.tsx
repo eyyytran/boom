3cad4ddf7acb60d86454eca7ea00f3ee1dce701a
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-19 05:05:17 -1000

    Update PrivateRoutes.tsx

--- client/src/components/PrivateRoutes.tsx
+++ client/src/components/PrivateRoutes.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { RootState } from '../store'
-import appSlice from '../store/appSlice'
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import userSlice from "../store/userSlice";
 
 const PrivateRoutes = () => {
-    const app = {
-        state: useSelector((state: RootState) => state.app),
-        actions: appSlice.actions,
-    }
-    const user = app.state.user
-    return user ? <Outlet /> : <Navigate to='/login' />
-}
+  const user = {
+    state: useSelector((state: RootState) => state.user),
+    actions: userSlice.actions,
+  };
+  const isUser = user.state.user;
+  return isUser ? <Outlet /> : <Navigate to="/login" />;
+};
 
-export default PrivateRoutes
+export default PrivateRoutes;
