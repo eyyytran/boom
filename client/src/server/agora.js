90e89846db053ba9a40e90b5b1f3a73983b94fd2
Blake Lein <blake.lein@gmail.com>
2022-09-20 08:30:16 -0500

    Updated morning token

--- client/src/server/agora.js
+++ client/src/server/agora.js
@@ -5,8 +5,7 @@ const appId = "871b434bcf5a497ead9d2160e52b815e";
 // This token is only good for four hours. Blake is looking into finding a more permanent solution.
 
 const token =
-  "007eJxTYHCd8a93YUx3ZfXfRXzhTHc/TGJPZGW7lfHzfeM85bSS1KUKDBbmhkkmxiZJyWmmiSaW5qmJKZYpRoZmBqmmRkkWhqapoZs0kq8yayUrKlQxMEIhiM/CkJuYmcfAAABtwx/U";
-
+  "007eJxTYJhX6fd08d0T/X8El08PnLEnUmfFjiIxlplVxyQ2bdN5LfpdgcHC3DDJxNgkKTnNNNHE0jw1McUyxcjQzCDV1CjJwtA09fh+zWR3Qe3kVLHdDIxQCOKzMOQmZuYxMAAAyKwgww==";
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
