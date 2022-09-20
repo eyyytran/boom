1491fc6634ecc80cc96ecb5c33eee44a9a50a03e
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-20 04:55:00 -1000

    Update Video.tsx

--- client/src/components/Video.tsx
+++ client/src/components/Video.tsx
@@ -10,12 +10,7 @@ import Component from "../components/Component";
 
 import { AgoraVideoPlayer } from "agora-rtc-react";
 
-import {
-  faMicrophone,
-  faMicrophoneSlash,
-  faVideoCamera,
-  faVideoSlash,
-} from "@fortawesome/free-solid-svg-icons";
+import { faMicrophone, faMicrophoneSlash, faVideoCamera, faVideoSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = {
@@ -36,19 +31,14 @@ type Styles = {
 
 const styles = {} as Styles;
 
-styles.static = "relative h-full";
+styles.static = "relative h-full md:h-auto aspect-video border border-red-500";
 
 styles.conditional = {
   active: "bg-violet-500 text-neutral-50",
   inactive: "bg-neutral-300",
 };
 
-export default function Video({
-  active,
-  className = null,
-  tracks,
-  videoTrack,
-}: Props) {
+export default function Video({ active, className = null, tracks, videoTrack }: Props) {
   const video = {
     state: useSelector((state: RootState) => state.video),
     actions: videoSlice.actions,
@@ -62,18 +52,11 @@ export default function Video({
     <Component id="Video">
       <div className={`${styles.static} ${styles.dynamic}`}>
         <div className="h-full w-full object-cover object-center rounded overflow-clip">
-          <AgoraVideoPlayer
-            videoTrack={videoTrack}
-            className="w-full h-full rounded scale-105"
-          />
+          <AgoraVideoPlayer videoTrack={videoTrack} className="w-full h-full rounded scale-105" />
         </div>
 
         <div className="absolute inset-0 flex justify-start items-end">
-          <div
-            className={`w-full p-2 flex justify-between items-center rounded-b ${
-              active ? styles.conditional.active : styles.conditional.inactive
-            }`}
-          >
+          <div className={`w-full p-2 flex justify-between items-center rounded-b ${active ? styles.conditional.active : styles.conditional.inactive}`}>
             <button
               className="w-8"
               onClick={async () => {
@@ -81,10 +64,7 @@ export default function Video({
                 await tracks[0].setEnabled(video.state.microphone);
               }}
             >
-              <FontAwesomeIcon
-                icon={video.state.microphone ? faMicrophoneSlash : faMicrophone}
-                className="text-xs text-inherit"
-              />
+              <FontAwesomeIcon icon={video.state.microphone ? faMicrophoneSlash : faMicrophone} className="text-xs text-inherit" />
             </button>
             <span className="text-xs text-inherit">Username</span>
             <button
@@ -94,10 +74,7 @@ export default function Video({
                 await tracks[1].setEnabled(video.state.camera);
               }}
             >
-              <FontAwesomeIcon
-                icon={video.state.camera ? faVideoSlash : faVideoCamera}
-                className="text-xs text-inherit"
-              />
+              <FontAwesomeIcon icon={video.state.camera ? faVideoSlash : faVideoCamera} className="text-xs text-inherit" />
             </button>
           </div>
         </div>
