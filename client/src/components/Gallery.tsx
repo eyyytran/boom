26b195acf5dca5bedb729f541542ebe765fe66c3
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-20 04:54:58 -1000

    Update Gallery.tsx

--- client/src/components/Gallery.tsx
+++ client/src/components/Gallery.tsx
@@ -91,9 +91,12 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
     <Component id="Gallery">
       <div ref={galleryRef} className={`${styles.static} ${styles.dynamic}`}>
         <Container>
-          <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center h-full gap-2 md:gap-3 lg:gap-4 border border-red-500">
+          <div className="flex flex-col md:grid md:grid-cols-2 justify-center items-center h-full gap-2 md:gap-3 lg:gap-4 border border-red-500">
             {video.state.start && tracks && (
               <div className="contents">
+                <Video tracks={tracks} videoTrack={tracks[1]} active={true} />
+                <Video tracks={tracks} videoTrack={tracks[1]} active={true} />
+                <Video tracks={tracks} videoTrack={tracks[1]} active={true} />
                 <Video tracks={tracks} videoTrack={tracks[1]} active={true} />
                 {video.state.users?.length > 0 &&
                   video.state.users.map(user => {
