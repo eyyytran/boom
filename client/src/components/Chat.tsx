39ec14fc2d23b32922331c5e0f6e9af8ba2f1b14
eyyytran <ltatran94@gmail.com>
2022-09-21 14:33:10 -0400

    stopped enter key drift

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -86,6 +86,7 @@ export default function Chat({ chatRef, className = null }: Props) {
     const handleKeyDown = (e: React.KeyboardEvent) => {
         e.stopPropagation()
         if (e.key === 'Enter') {
+            e.preventDefault()
             handleSubmit()
         }
     }
