e48864733ab1304bb8542a4058211c8cc22089c6
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-23 10:28:51 -1000

    Update Gallery.tsx

--- client/src/components/Gallery.tsx
+++ client/src/components/Gallery.tsx
@@ -79,7 +79,6 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
         init();
 
         client.on("user-published", async (user, mediaType) => {
-          alert(`user-publish ${user.uid}`);
           await client.subscribe(user, mediaType);
           if (mediaType === "video") {
             dispatch(video.actions.addUser(user));
@@ -90,7 +89,6 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
         });
 
         client.on("user-unpublished", (user, mediaType) => {
-          alert(`user-unpublish ${user.uid}`);
           if (mediaType === "audio") {
             if (user.audioTrack) user.audioTrack.stop();
           }
@@ -100,7 +98,6 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
         });
 
         client.on("user-left", user => {
-          alert(`user-left ${user.uid}`);
           dispatch(video.actions.removeUser(user));
         });
       } catch (error) {
@@ -113,8 +110,12 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
     const unpublish = async () => {
       await client.unpublish();
       await client.leave();
+      // const tracks = client.localTracks;
+      // tracks.forEach(track => {
+      //   console.log(track);
+      // });
       dispatch(video.actions.removeUser(user));
-      alert("Client Unpublished");
+      // alert("Client Unpublished");
     };
     return () => {
       try {
@@ -131,7 +132,7 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
     <Component id="Gallery">
       <div ref={galleryRef} className={`${styles.static} ${styles.dynamic}`}>
         <Container>
-          <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center h-full gap-2 md:gap-3 lg:gap-4 border border-red-500">
+          <div className={`flex portrait:flex-col ${video.state.users.length > 1 ? "lg:grid lg:grid-cols-2" : null} justify-center items-center h-full gap-2 md:gap-3 lg:gap-4 border border-red-500`}>
             {tracks && (
               <div className="contents">
                 <Video tracks={tracks} active={true} username={user.state.userName} />
