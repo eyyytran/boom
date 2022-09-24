badd81d58aea3c473658e6099de04e9953f09b48
Blake Lein <blake.lein@gmail.com>
2022-09-24 09:33:03 -0500

    Update modalSlice.ts

--- client/src/store/modalSlice.ts
+++ client/src/store/modalSlice.ts
@@ -1,23 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const modalSlice = createSlice({
-    name: 'modal',
-    initialState: {
-        isShowGivePointModal: false,
-        isShowIsTurnModal: false,
-        isShowWinnerModal: false,
+  name: "modal",
+  initialState: {
+    isShowGivePointModal: false,
+    isShowIsTurnModal: false,
+    isShowWinnerModal: false,
+  },
+  reducers: {
+    setIsShowGivePointModal: (state, { payload }) => {
+      state.isShowGivePointModal = payload;
     },
-    reducers: {
-        setIsShowGivePointModal: (state, { payload }) => {
-            state.isShowGivePointModal = payload
-        },
-        setIsShowIsTurnModal: (state, { payload }) => {
-            state.isShowIsTurnModal = payload
-        },
-        setIsShowWinnerModal: (state, { payload }) => {
-            state.isShowWinnerModal = payload
-        },
+    setIsShowIsTurnModal: (state, { payload }) => {
+      state.isShowIsTurnModal = payload;
     },
-})
+    setIsShowWinnerModal: (state, { payload }) => {
+      state.isShowWinnerModal = payload;
+    },
+  },
+});
 
-export default modalSlice
+export default modalSlice;
