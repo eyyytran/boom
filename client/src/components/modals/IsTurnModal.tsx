54fd7440d799188f06e61dfa169332f012474a7b
Blake Lein <blake.lein@gmail.com>
2022-09-25 07:12:04 -0500

    Update IsTurnModal.tsx

--- client/src/components/modals/IsTurnModal.tsx
+++ client/src/components/modals/IsTurnModal.tsx
@@ -1,21 +1,21 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../../store'
-import modalSlice from '../../store/modalSlice'
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
+import modalSlice from "../../store/modalSlice";
 
 const IsTurnModal = () => {
-    const modal = {
-        state: useSelector((state: RootState) => state.modal),
-        action: modalSlice.actions,
-    }
+  const modal = {
+    state: useSelector((state: RootState) => state.modal),
+    action: modalSlice.actions,
+  };
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            <button onClick={() => dispatch(modal.action.setIsShowIsTurnModal(false))}>X</button>
-            <h1>It's Your Turn!</h1>
-        </div>
-    )
-}
+  return (
+    <div className="bg-green-500 text-white text-center font-bold">
+      {/* <button onClick={() => dispatch(modal.action.setIsShowIsTurnModal(false))}>X</button> */}
+      <h1>It's Your Turn!</h1>
+    </div>
+  );
+};
 
-export default IsTurnModal
+export default IsTurnModal;
