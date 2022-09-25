75194469ae52ca0e7d212568ae2058352e53423c
eyyytran <ltatran94@gmail.com>
2022-09-25 16:39:06 -0400

    fixed join bug

--- client/src/components/Join.tsx
+++ client/src/components/Join.tsx
@@ -42,13 +42,11 @@ const Join = (props: Props) => {
                 const playerList = docSnap.data().gameState.players
                 const playerNum =
                     playerList.findIndex(
-                        (playerObj: any) =>
-                            playerObj.player === user.state.userName
+                        (playerObj: any) => playerObj.player === user.state.userName
                     ) === -1
                         ? playerList.length + 1
                         : playerList.findIndex(
-                              (playerObj: any) =>
-                                  playerObj.player === user.state.userName
+                              (playerObj: any) => playerObj.player === user.state.userName
                           )
                 dispatch(game.actions.setPlayerNum(playerNum))
                 dispatch(game.actions.setIsOwner(false))
@@ -64,6 +62,8 @@ const Join = (props: Props) => {
     const joinGame = (e: React.SyntheticEvent) => {
         e.preventDefault()
         updateRoom()
+        // setTimeout(() => {
+        // }, 700)
         navigate(`/boom/?id=${gameCode}`)
     }
 
@@ -71,9 +71,7 @@ const Join = (props: Props) => {
         <Component id='NewGame'>
             <div className='flex flex-col items-center h-full w-full'>
                 <div className='flex flex-col items-center m-16'>
-                    <h1 className='text-xl text-center font-bold text-gray-900'>
-                        Join A Game
-                    </h1>
+                    <h1 className='text-xl text-center font-bold text-gray-900'>Join A Game</h1>
                     <form onSubmit={joinGame}>
                         <input
                             type='text'
