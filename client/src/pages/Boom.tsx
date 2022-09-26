136b2d60c9ec86a0475a82fd9012c268dc16ce76
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-25 16:10:41 -1000

    Update Boom.tsx

--- client/src/pages/Boom.tsx
+++ client/src/pages/Boom.tsx
@@ -1,235 +1,208 @@
-import { useEffect, useRef, useState, useMemo } from 'react'
-import { doc, getDoc, onSnapshot } from 'firebase/firestore'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
-import { RootState } from '../store'
-import { db } from '../server/firebase'
-import userSlice from '../store/userSlice'
-import gameSlice from '../store/gameSlice'
-import modalSlice from '../store/modalSlice'
-import Component from '../components/Component'
-import Titlebar from '../components/Titlebar'
-import Timer from '../components/timer/Timer'
-import Gallery from '../components/Gallery'
-import Artboard from '../components/Artboard'
-import Chat from '../components/Chat'
-import Navbar from '../components/Navbar'
-import { useNavigate } from 'react-router-dom'
-import IParticipant from '../components/interfaces/IParticipant'
-import timerSlice from '../store/timerSlice'
+import { useEffect, useRef, useState, useMemo } from "react";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { RootState } from "../store";
+import { db } from "../server/firebase";
+import userSlice from "../store/userSlice";
+import gameSlice from "../store/gameSlice";
+import modalSlice from "../store/modalSlice";
+import Component from "../components/Component";
+import Titlebar from "../components/Titlebar";
+import Timer from "../components/timer/Timer";
+import Gallery from "../components/Gallery";
+import Artboard from "../components/Artboard";
+import Chat from "../components/Chat";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import IParticipant from "../components/interfaces/IParticipant";
+import timerSlice from "../store/timerSlice";
 
 type Styles = {
-    static: string
-}
+  static: string;
+};
 
-const styles = {} as Styles
+const styles = {} as Styles;
 
-styles.static = 'fixed inset-0 bg-neutral-200'
+styles.static = "fixed inset-0 bg-neutral-800";
 
 export default function Boom() {
-    const galleryRef = useRef<HTMLDivElement>(null)
-    const galleryButtonRef = useRef<HTMLDivElement>(null)
-    const artboardRef = useRef<HTMLDivElement>(null)
-    const artboardButtonRef = useRef<HTMLDivElement>(null)
-    const chatRef = useRef<HTMLDivElement>(null)
-    const chatButtonRef = useRef<HTMLDivElement>(null)
-    const exitButtonRef = useRef<HTMLDivElement>(null)
+  const galleryRef = useRef<HTMLDivElement>(null);
+  const galleryButtonRef = useRef<HTMLDivElement>(null);
+  const artboardRef = useRef<HTMLDivElement>(null);
+  const artboardButtonRef = useRef<HTMLDivElement>(null);
+  const chatRef = useRef<HTMLDivElement>(null);
+  const chatButtonRef = useRef<HTMLDivElement>(null);
+  const exitButtonRef = useRef<HTMLDivElement>(null);
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    console.log('REF GALLERY ', galleryRef)
-    console.log('REF ARTBOARD ', artboardRef)
-    console.log('REF CHAT ', chatRef)
+  console.log("REF GALLERY ", galleryRef);
+  console.log("REF ARTBOARD ", artboardRef);
+  console.log("REF CHAT ", chatRef);
 
-    // Viewport functionality
-    const isGalleryInView = useIsInViewport(galleryRef)
+  // Viewport functionality
+  const isGalleryInView = useIsInViewport(galleryRef);
 
-    const isArtboardInView = useIsInViewport(artboardRef)
+  const isArtboardInView = useIsInViewport(artboardRef);
 
-    const isChatInView = useIsInViewport(chatRef)
+  const isChatInView = useIsInViewport(chatRef);
 
-    function useIsInViewport(ref: any) {
-        const [isIntersecting, setIsIntersecting] = useState(false)
+  function useIsInViewport(ref: any) {
+    const [isIntersecting, setIsIntersecting] = useState(false);
 
-        const observer = useMemo(
-            () =>
-                new IntersectionObserver(([entry]) => setIsIntersecting(entry.isIntersecting), {
-                    threshold: 0.5,
-                }),
-            []
-        )
-
-        useEffect(() => {
-            observer.observe(ref.current)
-
-            return () => {
-                observer.disconnect()
-            }
-        }, [ref, observer])
-
-        return isIntersecting
-    }
-
-    const user = {
-        state: useSelector((state: RootState) => state.user),
-        action: userSlice.actions,
-    }
-
-    const game = {
-        state: useSelector((state: RootState) => state.game),
-        action: gameSlice.actions,
-    }
-
-    const modal = {
-        state: useSelector((state: RootState) => state.modal),
-        action: modalSlice.actions,
-    }
-
-    const timer = {
-        state: useSelector((state: RootState) => state.timer),
-        action: timerSlice.actions,
-    }
+    const observer = useMemo(
+      () =>
+        new IntersectionObserver(([entry]) => setIsIntersecting(entry.isIntersecting), {
+          threshold: 0.5,
+        }),
+      [],
+    );
 
     useEffect(() => {
-        if (!game.state.roomId) return
-        const getParticipants = async () => {
-            const docSnap = await getDoc(doc(db, 'rooms', game.state.roomId))
-            if (docSnap.exists()) {
-                const data = docSnap.data()
-                const participants = data.gameState.players
-                dispatch(game.action.setPlayers(participants))
-            }
-        }
+      observer.observe(ref.current);
 
-        const reassignPlayerNum = (playerList: IParticipant[]) => {
-            const index = playerList.findIndex(player => player.uid === user.state.user?.uid)
-            dispatch(game.action.setPlayerNum(index))
-            if (index === 0) {
-                dispatch(game.action.setIsOwner(true))
-            }
-        }
+      return () => {
+        observer.disconnect();
+      };
+    }, [ref, observer]);
 
-        const unsubscribe = onSnapshot(doc(db, 'rooms', game.state.roomId), doc => {
-            const data = doc.data()
-            const dbGameState = data?.gameState
+    return isIntersecting;
+  }
 
-            if (dbGameState.isEnded) {
-                navigate('/dashboard')
-                dispatch(game.action.resetState())
-                dispatch(modal.action.resetModals())
-                dispatch(timer.action.resetTime())
-                return
-            }
+  const user = {
+    state: useSelector((state: RootState) => state.user),
+    action: userSlice.actions,
+  };
 
-            dispatch(game.action.setIsInit(dbGameState.gameStarted))
-            dispatch(
-                modal.action.setIsShowIsTurnModal(
-                    dbGameState.gameStarted && dbGameState.whosTurn === game.state.playerNum
-                )
-            )
-            dispatch(
-                game.action.setIsTurn(
-                    dbGameState.gameStarted && dbGameState.whosTurn === game.state.playerNum
-                )
-            )
-            dispatch(game.action.setWhosTurn(dbGameState.whosTurn))
+  const game = {
+    state: useSelector((state: RootState) => state.game),
+    action: gameSlice.actions,
+  };
 
-            if (JSON.stringify(game.state.players) !== JSON.stringify(dbGameState.players)) {
-                getParticipants()
-                reassignPlayerNum(dbGameState.players)
-            }
+  const modal = {
+    state: useSelector((state: RootState) => state.modal),
+    action: modalSlice.actions,
+  };
 
-            dispatch(game.action.setIsTurnStarted(dbGameState.isTurnStart))
-            if (dbGameState.isTurnStart) {
-                dispatch(timer.action.setEndTime(dbGameState.turnEndTime))
-            }
+  const timer = {
+    state: useSelector((state: RootState) => state.timer),
+    action: timerSlice.actions,
+  };
 
-            dispatch(timer.action.setIsStopTimer(dbGameState.isStopTimer))
+  useEffect(() => {
+    if (!game.state.roomId) return;
+    const getParticipants = async () => {
+      const docSnap = await getDoc(doc(db, "rooms", game.state.roomId));
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        const participants = data.gameState.players;
+        dispatch(game.action.setPlayers(participants));
+      }
+    };
 
-            dispatch(game.action.setIsWon(dbGameState.gameWon))
-            dispatch(game.action.setWinner(dbGameState.winner ? dbGameState.winner.player : null))
+    const reassignPlayerNum = (playerList: IParticipant[]) => {
+      const index = playerList.findIndex(player => player.uid === user.state.user?.uid);
+      dispatch(game.action.setPlayerNum(index));
+      if (index === 0) {
+        dispatch(game.action.setIsOwner(true));
+      }
+    };
 
-            if (dbGameState.gameWon && dbGameState.winner) {
-                dispatch(game.action.setIsInit(false))
-                dispatch(modal.action.setIsShowWinnerModal(true))
-            }
-        })
-        return () => {
-            unsubscribe()
-        }
-    }, [
-        dispatch,
-        game.action,
-        game.state.playerNum,
-        game.state.players,
-        game.state.roomId,
-        modal.action,
-        timer.action,
-        user.state.userName,
-        navigate,
-    ])
+    const unsubscribe = onSnapshot(doc(db, "rooms", game.state.roomId), doc => {
+      const data = doc.data();
+      const dbGameState = data?.gameState;
 
-    useEffect(() => {
-        if (!galleryButtonRef.current) return
-        galleryButtonRef.current.onclick = () => {
-            galleryRef.current?.scrollIntoView()
-        }
-    }, [])
+      if (dbGameState.isEnded) {
+        navigate("/dashboard");
+        dispatch(game.action.resetState());
+        dispatch(modal.action.resetModals());
+        dispatch(timer.action.resetTime());
+        return;
+      }
 
-    useEffect(() => {
-        if (!artboardButtonRef.current) return
-        artboardButtonRef.current.onclick = () => {
-            artboardRef.current?.scrollIntoView()
-        }
-    }, [])
+      dispatch(game.action.setIsInit(dbGameState.gameStarted));
+      dispatch(modal.action.setIsShowIsTurnModal(dbGameState.gameStarted && dbGameState.whosTurn === game.state.playerNum));
+      dispatch(game.action.setIsTurn(dbGameState.gameStarted && dbGameState.whosTurn === game.state.playerNum));
+      dispatch(game.action.setWhosTurn(dbGameState.whosTurn));
 
-    useEffect(() => {
-        if (!chatButtonRef.current) return
-        chatButtonRef.current.onclick = () => {
-            chatRef.current?.scrollIntoView()
-        }
-    }, [])
+      if (JSON.stringify(game.state.players) !== JSON.stringify(dbGameState.players)) {
+        getParticipants();
+        reassignPlayerNum(dbGameState.players);
+      }
 
-    useEffect(() => {
-        if (!exitButtonRef.current) return
-        exitButtonRef.current.onclick = () => {
-            alert('exitButtonRef')
-        }
-    }, [])
+      dispatch(game.action.setIsTurnStarted(dbGameState.isTurnStart));
+      if (dbGameState.isTurnStart) {
+        dispatch(timer.action.setEndTime(dbGameState.turnEndTime));
+      }
 
-    return (
-        <Component id='Boom'>
-            <div className={`${styles.static}`}>
-                <div className='flex flex-col justify-start h-full'>
-                    <Titlebar className='shrink-0' />
-                    <Timer className='shrink-0' />
-                    <div className='flex lg:grid lg:grid-cols-3 h-full overflow-y-clip overflow-x-auto snap-x snap-mandatory no-scrollbar scroll-smooth border-4 border-blue-500'>
-                        <Gallery
-                            galleryRef={galleryRef}
-                            className='shrink-0 border border-red-500 snap-center lg:col-start-1 lg:col-span-full'
-                        />
-                        <Artboard
-                            artboardRef={artboardRef}
-                            className='shrink-0 lg:shrink border border-red-500 snap-center'
-                        />
-                        <Chat
-                            chatRef={chatRef}
-                            className='shrink-0 border border-red-500 snap-center'
-                        />
-                    </div>
-                    <Navbar
-                        galleryButtonRef={galleryButtonRef}
-                        artboardButtonRef={artboardButtonRef}
-                        chatButtonRef={chatButtonRef}
-                        exitButtonRef={exitButtonRef}
-                        isGalleryInView={isGalleryInView}
-                        isArtboardInView={isArtboardInView}
-                        isChatInView={isChatInView}
-                        className='shrink-0'
-                    />
-                </div>
-            </div>
-        </Component>
-    )
+      dispatch(timer.action.setIsStopTimer(dbGameState.isStopTimer));
+
+      dispatch(game.action.setIsWon(dbGameState.gameWon));
+      dispatch(game.action.setWinner(dbGameState.winner ? dbGameState.winner.player : null));
+
+      if (dbGameState.gameWon && dbGameState.winner) {
+        dispatch(game.action.setIsInit(false));
+        dispatch(modal.action.setIsShowWinnerModal(true));
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch, game.action, game.state.playerNum, game.state.players, game.state.roomId, modal.action, timer.action, user.state.userName, navigate]);
+
+  useEffect(() => {
+    if (!galleryButtonRef.current) return;
+    galleryButtonRef.current.onclick = () => {
+      galleryRef.current?.scrollIntoView();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!artboardButtonRef.current) return;
+    artboardButtonRef.current.onclick = () => {
+      artboardRef.current?.scrollIntoView();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!chatButtonRef.current) return;
+    chatButtonRef.current.onclick = () => {
+      chatRef.current?.scrollIntoView();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!exitButtonRef.current) return;
+    exitButtonRef.current.onclick = () => {
+      alert("exitButtonRef");
+    };
+  }, []);
+
+  return (
+    <Component id="Boom">
+      <div className={`${styles.static}`}>
+        <div className="flex flex-col justify-start h-full">
+          <Titlebar className="shrink-0" />
+          <Timer className="shrink-0" />
+          <div className="flex lg:grid lg:grid-cols-3 gap-2 h-full overflow-y-clip overflow-x-auto snap-x snap-mandatory no-scrollbar scroll-smooth">
+            <Gallery galleryRef={galleryRef} className="shrink-0 snap-center lg:col-start-1 lg:col-span-full" />
+            <Artboard artboardRef={artboardRef} className="shrink-0 lg:shrink snap-center" />
+            <Chat chatRef={chatRef} className="shrink-0 snap-center" />
+          </div>
+          <Navbar
+            galleryButtonRef={galleryButtonRef}
+            artboardButtonRef={artboardButtonRef}
+            chatButtonRef={chatButtonRef}
+            exitButtonRef={exitButtonRef}
+            isGalleryInView={isGalleryInView}
+            isArtboardInView={isArtboardInView}
+            isChatInView={isChatInView}
+            className="shrink-0"
+          />
+        </div>
+      </div>
+    </Component>
+  );
 }
