71379285ebdf3818809f353081c73b42bdeedb88
West <76145386+westmcmillen@users.noreply.github.com>
2022-09-26 05:59:14 -1000

    Update Gallery.tsx

--- client/src/components/Gallery.tsx
+++ client/src/components/Gallery.tsx
@@ -82,7 +82,7 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
                 });
                 dispatch(video.actions.addUser(user));
                 const publishSound = new Audio("/sounds/chat.mp3");
-                publishSound.volume = 0.25;
+                publishSound.volume = 0.15;
                 publishSound.play();
               } catch (error) {
                 console.log("HERE", error);
@@ -107,7 +107,7 @@ export default function Gallery({ galleryRef, className = "" }: Props) {
             if (mediaType === "video") {
               user.videoTrack && user.videoTrack.stop();
               const unpublishSound = new Audio("/sounds/unpublish.mp3");
-              unpublishSound.volume = 0.25;
+              unpublishSound.volume = 0.15;
               unpublishSound.play();
               dispatch(video.actions.removeUser(user));
             }
