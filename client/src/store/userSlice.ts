224b8c35f8092a25234787f68de6a04bc9552848
eyyytran <ltatran94@gmail.com>
2022-09-26 20:37:30 -0400

    setting initial state for user image

--- client/src/store/userSlice.ts
+++ client/src/store/userSlice.ts
@@ -1,36 +1,38 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { User } from "firebase/auth";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { User } from 'firebase/auth'
 
 interface ICurrentUser {
-  user: User | null;
-  userName: string;
-  image: string;
+    user: User | null
+    userName: string
+    image: string
 }
 
-type State = ICurrentUser;
+type State = ICurrentUser
 
 const initialState: ICurrentUser = {
-  user: null,
-  userName: "",
-  image: "",
-};
+    user: null,
+    userName: '',
+    image: '',
+}
 
 const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    setUser: (state: State, action: PayloadAction<any>) => {
-      state.user = action.payload;
-      if (!state.user) return;
-      state.userName = action.payload.displayName;
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (state: State, action: PayloadAction<any>) => {
+            state.user = action.payload
+            if (!state.user) return
+            state.userName = action.payload.displayName
+            if (!state.user.photoURL) return
+            state.image = state.user.photoURL
+        },
+        setUserName: (state: State, action) => {
+            state.userName = action.payload
+        },
+        setUserImage: (state: State, action) => {
+            state.image = action.payload
+        },
     },
-    setUserName: (state: State, action) => {
-      state.userName = action.payload;
-    },
-    setUserImage: (state: State, action) => {
-      state.image = action.payload;
-    },
-  },
-});
+})
 
-export default userSlice;
+export default userSlice
